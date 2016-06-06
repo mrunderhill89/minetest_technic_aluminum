5fbe778778924b90738f3f77262d0b6392b13ee5
mrunderhill89 <mrunderhill89@gmail.com>
2016-06-06 09:24:36 -0700

    Compatibility patch for other mods that add rubies and sapphires.
    Thanks to HybridDog for pointing this out. This update renames the mod
    folders for my ruby and sapphire mods so that they don't get mixed up
    with anyone else's, like Zeg9's Weird Ores mod (https://github.com/Zeg9
    /minetest-weirdores).
    
    Also added some aliases for that mod so that their rubies are replaced
    with mine. This seems to work in Worldgen but I haven't seen any rubies
    that have already been mined be replaced, so keep an eye out for that.

--- aluminum_sapphire/init.lua
+++ aluminum_sapphire/init.lua
@@ -7,16 +7,6 @@ minetest.register_craftitem(":technic_aluminum:sapphire", {
 	inventory_image = "aluminum_sapphire.png",
 })
 
---- Sapphire Dust
-minetest.register_craftitem(":technic_aluminum:sapphire_dust", {
-	description = S("Blue Corundum Dust"),
-	inventory_image = "aluminum_sapphire_dust.png",
-})
-technic.register_alloy_recipe({input = {"technic_aluminum:alumina_dust 3", "technic:wrought_iron_dust 1"}, output = "technic_aluminum:sapphire_dust", time = 12})
-technic.register_separating_recipe({input = {"technic_aluminum:sapphire_dust"}, output = {"technic_aluminum:alumina_dust 3", "technic:wrought_iron_dust 1"}, time = 12})
-technic.register_grinder_recipe({input = {"technic_aluminum:sapphire"}, output = "technic_aluminum:sapphire_dust 4", time = 12})
-technic.register_compressor_recipe({input = {"technic_aluminum:sapphire_dust 4"}, output = "technic_aluminum:sapphire", time = 12})
-
 --- Sapphire Block
 minetest.register_node(":technic_aluminum:sapphire_block", {
 	description = S("Sapphire Block"),
@@ -50,23 +40,35 @@ minetest.register_node(":technic_aluminum:stone_with_sapphires", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
-minetest.register_craft({
-	output = 'technic:blue_energy_crystal',
-	recipe = {
-		{'technic_aluminum:sapphire_block', 'technic:battery', 'dye:blue'},
-		{'technic:battery', 'technic:green_energy_crystal', 'technic:battery'},
-		{'dye:blue', 'technic:battery', 'technic_aluminum:sapphire_block'},
-	}
-})
+if technic then
+  --- Sapphire Dust
+  minetest.register_craftitem(":technic_aluminum:sapphire_dust", {
+    description = S("Blue Corundum Dust"),
+    inventory_image = "aluminum_sapphire_dust.png",
+  })
+  technic.register_alloy_recipe({input = {"technic_aluminum:alumina_dust 3", "technic:wrought_iron_dust 1"}, output = "technic_aluminum:sapphire_dust", time = 12})
+  technic.register_separating_recipe({input = {"technic_aluminum:sapphire_dust"}, output = {"technic_aluminum:alumina_dust 3", "technic:wrought_iron_dust 1"}, time = 12})
+  technic.register_grinder_recipe({input = {"technic_aluminum:sapphire"}, output = "technic_aluminum:sapphire_dust 4", time = 12})
+  technic.register_compressor_recipe({input = {"technic_aluminum:sapphire_dust 4"}, output = "technic_aluminum:sapphire", time = 12})
 
-minetest.register_craft({
-	output = 'technic:water_mill',
-	recipe = {
-		{'technic:marble', 'technic_aluminum:sapphire',        'technic:marble'},
-		{'group:wood',     'technic:machine_casing', 'group:wood'},
-		{'technic:marble', 'technic:lv_cable0',      'technic:marble'},
-	}
-})
+  minetest.register_craft({
+    output = 'technic:blue_energy_crystal',
+    recipe = {
+      {'technic_aluminum:sapphire_block', 'technic:battery', 'dye:blue'},
+      {'technic:battery', 'technic:green_energy_crystal', 'technic:battery'},
+      {'dye:blue', 'technic:battery', 'technic_aluminum:sapphire_block'},
+    }
+  })
+
+  minetest.register_craft({
+    output = 'technic:water_mill',
+    recipe = {
+      {'technic:marble', 'technic_aluminum:sapphire',        'technic:marble'},
+      {'group:wood',     'technic:machine_casing', 'group:wood'},
+      {'technic:marble', 'technic:lv_cable0',      'technic:marble'},
+    }
+  })
+end
 
 minetest.register_ore({
   ore_type       = "scatter",
